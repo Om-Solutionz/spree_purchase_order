b1a38b3e9d5ac0c5010119f42348dd5487a8ce83
umair azam <umairrazam@gmail.com>
2021-01-28 20:20:18 +0500

    fix(specs): update rails version

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,7 +1,11 @@
-Spree::Payment.class_eval do
-  scope :from_purchase_order, -> { where(source_type: 'Spree::PurchaseOrder') }
-
-  def po?
-    source_type == 'Spree::PurchaseOrder'
-  end
+module Spree
+	module PaymentDecorator
+	    def self.prepended(base)
+			base.scope :from_purchase_order, -> { where(source_type: 'Spree::PurchaseOrder') }
+		end
+		def po?
+			source_type == 'Spree::PurchaseOrder'
+		end
+	end
 end
+::Spree::Payment.prepend(Spree::PaymentDecorator)
